02e7e1c043a8f03a4b5afc91c780ca51e654dcd9
jasurgraduate <jasur@graduate.org>
2024-11-29 16:34:47 +0900

    edited :)

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -1,13 +1,27 @@
 // src/components/MusicPlayer.js
 import React from 'react';
 import Player from './Player';
-import UpDown from './UpDown'; // Import UpDown component
+import UpDown from './UpDown';
+import Snowfall from "react-snowfall";
 
 const MusicPlayer = () => {
     return (
         <div>
+            <Snowfall
+                color="white"       // Snowflake color
+                snowflakeCount={100} // Number of snowflakes
+                radius={[0, 6]}      // Radius (size range) of snowflakes (ensure this is an array)
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    zIndex: 1000,        // Ensure snowfall stays behind content
+                }}
+            />
             <Player />
-            <UpDown /> {/* Add the UpDown component */}
+            <UpDown /> {/* UpDown component */}
         </div>
     );
 };
