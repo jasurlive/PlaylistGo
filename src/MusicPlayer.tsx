5cc519c88b210dbb9ae2b98e3515a54225ba530b
jasurgraduate <jasur@graduate.org>
2025-03-30 15:24:19 +0900

    social icons added

--- src/MusicPlayer.tsx
+++ src/MusicPlayer.tsx
@@ -4,12 +4,14 @@ import SnowFall from "./add/tools/basic/SnowFall";
 import Online from "./add/tools/basic/Online";
 import Footer from "./add/tools/basic/Footer";
 import Header from "./add/tools/basic/Header";
+import Social from "./add/tools/basic/Social";
 
 const MusicPlayer = () => {
   return (
     <div>
       <Header />
       <Player />
+      <Social />
     </div>
   );
 };
